d74026ae8f5840b931ccd20f732ba1330b25153c
Laszlo Kocsis <kocsislaci95@gmail.com>
2022-05-27 14:59:26 +0200

    initial commit

---
+++ mylibrary/src/main/java/hu/riltech/mylibrary/HelloJitpack.kt
@@ -0,0 +1,10 @@
+package hu.riltech.mylibrary
+
+import android.util.Log
+import android.util.Log.INFO
+
+object HelloJitpack {
+    public fun HelloJitpack() {
+        Log.i(null, "Hello Jitpack")
+    }
+}
